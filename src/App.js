e56136bd1a8575c7cfcb71e2bbdfa3fb10a69172
Artem <unnamed_9595@mail.ru>
2020-09-03 13:38:40 +0300

    add anim

--- src/App.js
+++ src/App.js
@@ -2,12 +2,24 @@ import React from 'react';
 import './App.css';
 import Burger from './Burger/Burger'
 
-function App() {
-  return (
-    <div className="App">
-      <Burger />
-    </div>
-  );
+class App extends React.PureComponent {
+  state = {
+    isOpen: true,
+  }
+
+  handleClick = () => {
+    this.setState({ isOpen: !this.state.isOpen });
+  }
+
+  render() {
+    const { isOpen } = this.state;
+    return (
+      <div className="App">
+        <Burger isOpen={isOpen} onClick={this.handleClick} />
+      </div>
+    );
+  }
 }
+
 
 export default App;
--- src/Burger/Burger.js
+++ src/Burger/Burger.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './Burger.css';
 
-function Burger() {
+function Burger({ isOpen, onClick }) {
   return (
-    <div className="root">
-      <svg className="close" version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 52 51.8">
+    <div className="root" onClick={onClick}>
+      <svg className={`${!isOpen ? 'closeHide' : 'close'}`} version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 52 51.8">
         <path d="M7.7,7.8L26,26.1" />
         <path d="M7.5,44.5l17-17"/>
         <path d="M44.4,44.5L26,26.1"/>
